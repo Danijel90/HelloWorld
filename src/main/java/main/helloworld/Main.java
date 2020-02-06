02d26ea916b3f5e547e246021e62fc5328d6c6c9
Danijel Minic <minic.danijel@gmai.com>
2020-02-06 19:58:50 +0100

    adds examples for differnet loop types

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -1,6 +1,7 @@
 package main.helloworld;
 
 import java.util.Scanner;
+import jdk.nashorn.api.tree.ForInLoopTree;
 
 /**
  *
@@ -12,74 +13,138 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        System.out.println("Hello World");
-        System.out.println("Danijel Minic");
-        
-        int age = 5;
-        
-        if (age > 18) {
-            System.out.println("Osoba je Punoletna");
-        } else {
-             System.out.println("Osoba je Maloletna");
+//        
+//        System.out.println("Hello World");
+//        System.out.println("Danijel Minic");
+//        
+//        int age = 5;
+//        if (age > 18){
+//            System.out.println("Osoba je punoletna");
+//        } else {
+//            System.out.println("Osoba je maloletna");
+//        } 
+//              
+//        System.out.print("Type your age number");
+//        Scanner input = new Scanner(System.in);
+//        int userAge = input.nextInt(); 
+//       
+//        System.out.println("Korisnik je uneo: " + userAge);
+//        
+//        if (userAge > 18){
+//            System.out.println("Osoba je punoletna");
+//        } else {
+//             System.out.println("Osoba je maloletna");
+//        }
+//        
+//        
+//       
+//        if (userAge >= 0 && userAge <=5 ) {
+//            System.out.println("Baby");
+//        } else if (userAge >=6 && userAge <=11) {
+//            System.out.println("Kid");
+//        } else if (userAge >=12 && userAge <=17){
+//            System.out.println("Teen");
+//        } else if (userAge >18 && userAge <=150s) {
+//            System.out.println("Adult");
+//        } else if {
+//            System.out.println("Invalid age number");
+//        } 
+//       
+//       
+//      System.out.print("Unesiti redni broj dana u nedelji (1-7): ");
+//        Scanner input3 = new Scanner(System.in);
+//        int dayNumber = input3.nextInt();
+//        String day;
+//        switch (dayNumber) {
+//           case 1:
+//             day = "Ponedeljak";
+//             break;
+//           case 2:
+//             day = "Utorak";
+//             break;
+//           case 3:
+//             day = "Sreda";
+//             break;
+//           case 4:
+//             day = "Četvrtak";
+//             break;
+//           case 5:
+//             day = "Petak";
+//             break;
+//           case 6:
+//             day = "Subota";
+//             break;
+//           case 7:
+//             day = "Nedelja";
+//             break;
+//           default:
+//             day = "Netačan unos";
+//             break;
+//        }
+//        System.out.println(day);
+
+//
+//     int x = 5;
+//     while(x > 0) {
+//         System.out.println(x);
+//         x = x + 1;
+//     }
+     
+//     System.out.println("While loop222:");
+//       x = 10;
+//        while(x > 0){
+//            System.out.println(x);
+//            x--;
+//        }
+//        System.out.println("Do While Loop:");
+//        x = 20;
+//                do {
+//                    System.out.println(x);
+//                    x--;
+//                } while(x > 15);
+
+       
+     System.out.println("For Loop:");
+        for(int i = 50; i < 60; i++) {
+            System.out.println(i);
         }
-        System.out.println("Unesite broj godina");
-        Scanner input = new Scanner (System.in);
-        
-        int userAge = input.nextInt();
-        
-        
-        System.out.println("Korisnik je uneo : " + userAge);
-           if (userAge > 18) {
-            System.out.println("Osoba je Punoletna");
-        } else {
-             System.out.println("Osoba je Maloletna");
-        
-             
-               System.out.println("..............................."); 
-               
-               System.out.println("Unesite broj godina");
-               Scanner input2 = new Scanner(System.in);
+        System.out.println("For loop2:");
+        for(int i = 30; i >= 20; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Kvadrat: ");
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j< 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("Koordinate 1: ");
+        for (int i = 1; i <=10; i++) {
+            for (int j = 1; j<= 10; j++) {
+                System.out.print("("+ i + "-" + j + ") ");
+            }
+            System.out.println();
+        }          
+               System.out.println("Kordinate2");        
            
-               int personAge = input2.nextInt();
-                
-               
-               if (personAge <= 0) {
-                   System.out.println("Invalid");
-                 }
-               else if (personAge <=5)    
-                        if (personAge <= 0) {
-                   System.out.println("Baby");
-                 }
-               else if (personAge <=11)    
-                                  if (personAge <= 0) {
-                   System.out.println("Kid");
-                 }
-               else if (personAge <=17)    
-                        if (personAge <= 0)
-                              
-                           
-                           
-                           
-                           
-                 
-                       
-                       
-                       
-                       
-                       
-                       
-                       
-               
-               
-             
-        
+              System.out.println("Koordinate 1: ");
+        for (int i = 1; i <=10; i++) {
+            for (int j = 1; j<= 10; j++) {
+                System.out.print("("+ i + "-" + j + ") ");
+            }
+            System.out.println();   
         }
 
+    
+    
+    
+    
     }
+    
+
 
 }
-    
            
     
            
